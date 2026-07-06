65970a452552fef889f22ad3586aad18682035f6
Rodrigo <rodrigo.santos12ri@gmail.com>
2026-07-06 00:37:47 -0300

    Corrige conexão com Supabase

--- database.py
+++ database.py
@@ -1,21 +1,6 @@
-import socket
-socket.getaddrinfo = lambda *args, **kwargs: socket._socket.getaddrinfo(*args, **kwargs)
 import os
 import psycopg2
-from psycopg2.extras import RealDictCursor
 
 def conectar():
-
-    conn = psycopg2.connect(
-        host="db.rhdwmrhusrnvdlxqawht.supabase.co",
-        dbname="postgres",
-        user="postgres",
-        password=os.getenv("DB_PASSWORD"),
-        port=5432,
-        sslmode="require",
-        connect_timeout=20,
-        cursor_factory=RealDictCursor,
-        options="-c client_encoding=UTF8"
-    )
-
+    conn = psycopg2.connect(os.getenv("DATABASE_URL"))
     return conn
