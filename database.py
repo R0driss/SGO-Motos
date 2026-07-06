c1ecc920f0e83ed5ece2c64cec3f0ebdbde49063
Rodrigo <rodrigo.santos12ri@gmail.com>
2026-07-06 00:02:52 -0300

    fix render supabase ipv4 connection

--- database.py
+++ database.py
@@ -1,3 +1,5 @@
+import socket
+socket.getaddrinfo = lambda *args, **kwargs: socket._socket.getaddrinfo(*args, **kwargs)
 import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
@@ -6,13 +8,14 @@ def conectar():
 
     conn = psycopg2.connect(
         host="db.rhdwmrhusrnvdlxqawht.supabase.co",
-        database="postgres",
+        dbname="postgres",
         user="postgres",
         password=os.getenv("DB_PASSWORD"),
         port=5432,
         sslmode="require",
+        connect_timeout=20,
         cursor_factory=RealDictCursor,
-        connect_timeout=20
+        options="-c client_encoding=UTF8"
     )
 
     return conn
